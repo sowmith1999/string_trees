cd572876c5ab00394ac98f0cb022ba873c89ab51
sowmith1999 <sowmithkunapaneni@gmail.com>
2024-07-29 07:43:59 -0500

    changing the update_augments

--- avl_tree_v2.cpp
+++ avl_tree_v2.cpp
@@ -1,12 +1,12 @@
 // Sequence Binary Tree - AVL Tree
-// stores strings, one string is one unit, characters don't exist
+#include <cassert>
 #include <cstdint>
-#include <string>
+#include <iostream>
 
 typedef struct AVLNode {
     uint32_t height = 0;
     uint32_t size = 1;
-    std::string* val_str = nullptr;
+    int32_t val = -1;
     AVLNode* parent = nullptr;
     AVLNode* left = nullptr;
     AVLNode* right = nullptr;
@@ -24,40 +24,57 @@ uint32_t get_size(AVLNode* node) {
     return 0;
 }
 
+uint32_t compute_skew(AVLNode* node) {
+    assert(node != nullptr && "Compute skew has a nullptr node");
+    return get_height(node->left) - get_height(node->right);
+}
+
 typedef struct AVLTree {
     AVLNode* root = nullptr;
 } AVLTree;
 
-AVLNode* init_AVLNode(std::string* val_str) {
+AVLNode* init_AVLNode(int32_t val) {
     AVLNode* node = new AVLNode();
-    node->val_str = val_str;
+    node->val = val;
     return node;
 }
 
 // Updates size along the spine
-// Can just do a simple +1 along the spine, I think
+// Here changing the size update to add left + right + 1, 
+//      instead of just +1, which doesn't work for rotations.
 void update_augments(AVLNode* naya) {
     if (naya->parent == nullptr)
         return;
     AVLNode* cur = naya->parent;
     while (cur != nullptr) {
-        cur->size += 1;
+        // cur->size += 1;
+        cur->size = get_size(cur->left) + get_size(cur->right) + 1;
         cur->height = std::max(get_height(cur->left), get_height(cur->right)) + 1;
         cur = cur->parent;
     }
 }
 
-void insert_node(AVLTree* tree, const std::string& str, uint32_t idx) {
-    std::string* val_str = new std::string(str);
-    AVLNode* naya = init_AVLNode(val_str);
+AVLNode* subtree_at(AVLNode* node, uint32_t idx) {
+    uint32_t soize = node->left->size;
+    if (idx < soize)
+        return subtree_at(node->left, idx);
+    else if (idx > soize)
+        return subtree_at(node->right, idx - soize - 1);
+    return node;
+}
+
+void insert_node(AVLTree* tree, int32_t val, uint32_t idx) {
+    AVLNode* naya = init_AVLNode(val);
     AVLNode* cur = nullptr;
     AVLNode* temp = tree->root;
     while (temp != nullptr) {
         cur = temp; // stores the to be parent
-        if (idx < cur->size)
+        if (idx < cur->size) {
             temp = temp->left;
-        else
+        } else {
             temp = temp->right;
+            idx = idx - cur->size - 1;
+        }
     }
     naya->parent = cur;
     if (cur == nullptr)
@@ -66,12 +83,55 @@ void insert_node(AVLTree* tree, const std::string& str, uint32_t idx) {
         cur->left = naya;
     else
         cur->right = naya;
+
     update_augments(naya);
+}
+
+void transplant(AVLTree* tree, AVLNode* original, AVLNode* naya) {
+    if (original == tree->root)
+        tree->root = naya;
+    else if (original->parent->left == original)
+        original->parent->left = naya;
+    else if (original->parent->right == original)
+        original->parent->right = naya;
+    if (naya != nullptr)
+        naya->parent = original->parent;
+}
+
+// rot
+//      : True for right rotate
+//      : False for left rotate
+void rotate(AVLTree* tree, AVLNode* node, bool rot) {
+    // right rotate
+    if (rot == true) {
+        // left child definitely exists
+        AVLNode* rep_node = node->left;
+        node->left = rep_node->right;
+        rep_node->right = node;
+        transplant(tree, node, rep_node);
+        node->parent = rep_node;
+    }
+    // left rotate
+    else if (rot == false) {
+        AVLNode* rep_node = node->right;
+        node->right = rep_node->left;
+        rep_node->left = node;
+        transplant(tree, node, rep_node);
+        node->parent = rep_node;
+    }
+    update_augments(node);
 }
 
 int main() {
     AVLTree* tree = new AVLTree();
-    insert_node(tree, "Hello World!", 0);
-    insert_node(tree, "well", 0);
-    insert_node(tree, "beaut", 0);
+    insert_node(tree, 1, 0);
+    insert_node(tree, 2, 1);
+    insert_node(tree, 3, 2);
+    rotate(tree, tree->root, false);
+    insert_node(tree, 4, 3);
+    insert_node(tree, 5, 4);
+    rotate(tree, tree->root, false);
+    printf("Boom\n");
+    printf("The root is %d\n", tree->root->val);
+    std::cout << subtree_at(tree->root, 2)->val << std::endl;
 }
